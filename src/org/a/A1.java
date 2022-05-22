ef1817bf453a3fe222a3688c6ee8c98fe1f64790
Deepashivadas <deepashivadas1992@gmail.com>
2022-05-22 08:44:14 +0530

    hello

---
+++ src/org/a/A1.java
@@ -0,0 +1,13 @@
+package org.a;
+
+public class A1 {
+	static int sum;
+public int add(int a,int b) {
+	
+	sum=a+b;
+	return sum;
+	
+		
+
+}
+}
---
+++ src/org/a/B1.java
@@ -0,0 +1,19 @@
+package org.a;
+
+public class B1 extends A1{
+	public static void main(String[] args) {
+		B1 b=new B1();
+	sum=b.add(25, 25);
+	System.out.println(sum);
+	if (sum==50) {
+		System.out.println("valid");
+		
+	}
+	else {
+		System.out.println("invalid");
+	}
+	
+	}
+}
+
+
